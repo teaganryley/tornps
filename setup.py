c7e87e78720f31563ec7fe2399733f1f466e63d2
teaganryley <teaganschultz@gmail.com>
2020-04-14 21:18:30 -0400

    setup.py add for pytest test discovery

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="tornps", packages=find_packages())
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,8 @@
 import pytest
-import sys,os
+import sys
+import os
 
-# ensures pytest will locate project without having to run setup.py
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import src.tornps.app as app
+from src.tornps import app
 
 def test_dummy():
     t = app.dummy()
